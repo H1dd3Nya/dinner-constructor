a9b8c84c7cf85013930482f6c1393e566cc8bb5c
H1dd3N <gumskull102012@yandex.ru>
2024-03-14 07:49:55 +0300

    Added *checkType* and *addTypeAndDish* methods to DinnerConstructor class

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -1,5 +1,40 @@
 package ru.practicum.dinner;
 
-public class DinnerConstructor {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
+public class DinnerConstructor {
+    HashMap<String, ArrayList<String>> dishList;
+    ArrayList<String> dishes;
+    Random rd;
+
+    DinnerConstructor() {
+        dishList = new HashMap<>();
+        dishes = new ArrayList<>();
+        rd = new Random();
+    }
+
+    boolean checkType(String type) {
+        return dishList.containsKey(type);
+    }
+
+    void addTypeAndDish(String dishType, String dishName) {
+        boolean typeIsExist = checkType(dishType);
+
+        if (typeIsExist) {
+            dishes = new ArrayList<>();
+            dishes = dishList.get(dishType);
+            dishes.add(dishName);
+            dishList.put(dishType, dishes);
+        } else {
+            dishes.add(dishName);
+        }
+
+        dishList.put(dishType, dishes);
+    }
+
+    void combosGenerator (int numberOfCombos, ArrayList<String> dishTypes) {
+
+    }
 }
